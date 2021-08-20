ac06e4bac48905ae53ae3eeb83dc6e0a297b4ab7
Mitch Kimball <kimbalm2@wwu.edu>
2021-08-20 13:04:07 -0700

    Added image drawables for the dice roller. Added code logic to display the dice based on the roll.

--- Android-Basics-in-Kotlin/Unit1-KotlinBasics/Pathway4/DiceRoller/app/src/main/java/myprojects/diceroller/MainActivity.kt
+++ Android-Basics-in-Kotlin/Unit1-KotlinBasics/Pathway4/DiceRoller/app/src/main/java/myprojects/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package myprojects.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 /**
  * This activity allows the user to roll a dice and view the result
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+        rollDice()
     }
     /**
      * Roll the dice and update the screen with the result.
@@ -22,8 +23,16 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 /**
